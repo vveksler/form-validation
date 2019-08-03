52cb9fb9e8cfee126fe75d0e1b561dd4738c53f9
Vadim Veksler <reyzele91@gmail.com>
2019-08-03 13:38:34 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Form from './components/Form';
+import './index.css';
+
+ReactDOM.render(<Form />, document.getElementById('root'));
